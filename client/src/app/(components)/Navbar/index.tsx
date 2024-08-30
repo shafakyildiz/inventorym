a23e0a04bbffdd979bbc0bea067122ef71673b4b
shafakyildiz <shafakyildiz@gmail.com>
2024-08-30 11:01:49 +0300

    feat: implement bell icon to search bar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Bell, Menu } from "lucide-react";
 
 const Navbar = () => {
   return (
@@ -13,6 +13,18 @@ const Navbar = () => {
         >
           <Menu className="w-full h-4" />
         </button>
+      </div>
+
+      <div className="relative">
+        <input
+          type="search"
+          placeholder="Start type to search groups & products"
+          className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-3000 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+        />
+
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <Bell className="text-gray-500" size={20} />
+        </div>
       </div>
     </div>
   );
