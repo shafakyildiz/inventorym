79389007802eeac7607e935a1c9965af5e3d6d2c
shafakyildiz <shafakyildiz@gmail.com>
2024-08-30 10:58:49 +0300

    feat: use client component to use onClick

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client";
 
-type Props = {};
+import { Menu } from "lucide-react";
 
-const Navbar = (props: Props) => {
-  return <div>Navbar</div>;
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center w-full mb-7">
+      {/* LEFT SIDE */}
+      <div className="flex justify-between items-center gap-5">
+        <button
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-full h-4" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
