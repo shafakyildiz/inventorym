4d5d44d67bf1e1d0f93639823f04986af5f51fca
shafakyildiz <shafakyildiz@gmail.com>
2024-08-31 13:55:44 +0300

    fix: move index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,6 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // ROUTES
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
 
 // SERVER
 const port = process.env.PORT || 3001;
