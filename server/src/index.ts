746a399b7cfed680e89a5a6bbe1c9bf41efeac2c
shafakyildiz <shafakyildiz@gmail.com>
2024-09-07 10:54:15 +0300

    chore: aws configs

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,8 +30,8 @@ app.use("/users", userRoutes);
 app.use("/expenses", expenseRoutes);
 
 // SERVER
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on ${port}`);
 });
